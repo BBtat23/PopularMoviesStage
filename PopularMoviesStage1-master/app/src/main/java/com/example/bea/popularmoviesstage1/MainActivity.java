516333d02fb1d8295c3f458d9418b1eac662e826
BBtat23 <31885469+BBtat23@users.noreply.github.com>
2018-05-16 14:21:13 +0200

    Update MainActivity.java

--- PopularMoviesStage1-master/app/src/main/java/com/example/bea/popularmoviesstage1/MainActivity.java
+++ PopularMoviesStage1-master/app/src/main/java/com/example/bea/popularmoviesstage1/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -33,21 +34,28 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.List
     private List<Movie> movies = new ArrayList<>();
     private MovieAdapter mAdapter;
     private RecyclerView mRecyclerView;
-
-
+    private int sortCriteria;
+    private final String SORTCRITERIA = "sortCriteria";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        if (savedInstanceState != null){
+            savedInstanceState.getInt(SORTCRITERIA);
+        }
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_id);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         mRecyclerView.setLayoutManager(layoutManager);
         mAdapter = new MovieAdapter(this,movies,this);
         mRecyclerView.setAdapter(mAdapter);
-        new MovieAsyncTask().execute("popular");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SORTCRITERIA, sortCriteria);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -172,10 +180,15 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.List
         int itemId = item.getItemId();
         if (itemId == R.id.popular) {
             new MovieAsyncTask().execute("popular");
+            sortCriteria = 1;
+
         } else if (itemId == R.id.topRated) {
             new MovieAsyncTask().execute("top_rated");
+            sortCriteria = 2;
+
         }else if (itemId == R.id.favourites){
             queryFavourite();
+
         }
         return super.onOptionsItemSelected(item);
     }
